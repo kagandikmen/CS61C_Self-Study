1872976126234c345388420b0fd42c272fd086a1
Kagan Dikmen <kagandikmen@outlook.com>
2023-12-10 17:07:29 +0100

    worked on lab03 - KD
    I had a problem while trying to connect Venus to my local files, so I couldn't work on Exercise 4 (cc_test.s)

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 7
 
 .text
 main:
@@ -21,4 +21,18 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+    addi t0, x0, 1  # initialising t0 with 1 (base value of mul)
+loop:
+    beq a0, x0, exit
+    mul t0, t0, a0
+    addi a0, a0, -1
+    j loop
+
+exit:
+    mv a0, t0
+    jr ra       # could also have written ret
+
+    
+
+
+
