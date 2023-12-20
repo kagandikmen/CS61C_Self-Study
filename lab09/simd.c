46e3a1cb13cd29c2f2a2c74f1f5f5e3414696287
Kagan Dikmen <kagandikmen@outlook.com>
2023-12-20 04:08:39 +0100

    completed lab09

--- lab09/simd.c
+++ lab09/simd.c
@@ -52,10 +52,42 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
 	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
 	
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		/* YOUR CODE GOES HERE */
 
-		/* You'll need a tail case. */
+		__m128i sum_vector = _mm_setzero_si128();		// declaring and initialising the sum vector
+		__m128i buffer, compare;						// these could also be initialised with zeroes, but
+														// not absolutely necessary, they are written over in
+														// the inner for-loop
 
+		int p[4]; 										// I used "int *p;" here once, which caused a bus error
+
+		for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4)
+		{
+			buffer = _mm_loadu_si128((__m128i *)(vals + i));	// stores the next 4 elements of the array
+			
+			compare = _mm_cmpgt_epi32(buffer, _127);			// if the element of the buffer vector is 
+																// greater than 127, than compare will have
+																// ones at the same place, if otherwise then zeroes
+			
+			buffer = _mm_and_si128(buffer, compare);			// eliminating the values in the buffer, that
+																// are less than 128 (I actually don't know why
+																// we do that)
+			
+			sum_vector = _mm_add_epi32(buffer, sum_vector);		// adding to the sum_vector
+
+		}
+
+		_mm_storeu_si128((__m128i *)p, sum_vector);		// sum_vector will be zeroed out once we go into the
+														// outer for-loop again, so we need to save the result now
+
+		for (unsigned int i = 0; i < 4; i++)
+			result += p[i];
+
+		// Tail case:		
+		for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++)
+		{
+			if (vals[i] >= 128) result += vals[i];
+		}
+		
 	}
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
@@ -67,10 +99,47 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
 	__m128i _127 = _mm_set1_epi32(127);
 	long long int result = 0;
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		/* COPY AND PASTE YOUR sum_simd() HERE */
-		/* MODIFY IT BY UNROLLING IT */
 
-		/* You'll need 1 or maybe 2 tail cases here. */
+		__m128i sum_vector = _mm_setzero_si128();
+		__m128i buffer, compare;
+
+		int p[4];
+
+		// Unrolling 4 times:
+		for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16)
+		{
+			buffer = _mm_loadu_si128((__m128i *)(vals + i));
+			compare = _mm_cmpgt_epi32(buffer, _127);
+			buffer = _mm_and_si128(buffer, compare);
+			sum_vector = _mm_add_epi32(buffer, sum_vector);
+
+			buffer = _mm_loadu_si128((__m128i *)(vals + i + 4));
+			compare = _mm_cmpgt_epi32(buffer, _127);
+			buffer = _mm_and_si128(buffer, compare);
+			sum_vector = _mm_add_epi32(buffer, sum_vector);
+
+			buffer = _mm_loadu_si128((__m128i *)(vals + i + 8));
+			compare = _mm_cmpgt_epi32(buffer, _127);
+			buffer = _mm_and_si128(buffer, compare);
+			sum_vector = _mm_add_epi32(buffer, sum_vector);
+
+			buffer = _mm_loadu_si128((__m128i *)(vals + i + 12));
+			compare = _mm_cmpgt_epi32(buffer, _127);
+			buffer = _mm_and_si128(buffer, compare);
+			sum_vector = _mm_add_epi32(buffer, sum_vector);
+
+		}
+
+		_mm_storeu_si128((__m128i *)p, sum_vector);
+
+		for (unsigned int i = 0; i < 4; i++)
+			result += p[i];
+
+		// Tail case:		
+		for (unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++)
+		{
+			if (vals[i] >= 128) result += vals[i];
+		}
 
 	}
 	clock_t end = clock();
